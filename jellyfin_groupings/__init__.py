b0579e02b846541e0212a4e3f9fbae3001036405
ItsJust Improvement Bot 1780956202 <bot+1780956202@users.noreply.github.com>
2026-09-13 06:34:55 +0200

    Fix circular and missing imports in jellyfin_groupings

--- jellyfin_groupings/__init__.py
+++ jellyfin_groupings/__init__.py
@@ -1,8 +1,5 @@
-from jellyfin_groupings import JellyfinClient, group_movies_by_tag, group_movies_by_genre, main
+from jellyfin_groupings.client import JellyfinClient
+from jellyfin_groupings.groupings import group_movies_by_tag, group_movies_by_genre
+from jellyfin_groupings.main import JellyfinGroupings, main
 
-__all__ = [
-    "JellyfinClient",
-    "group_movies_by_tag",
-    "group_movies_by_genre",
-    "main",
-]
+__all__ = ['JellyfinClient', 'group_movies_by_tag', 'group_movies_by_genre', 'JellyfinGroupings', 'main']
---
+++ worker.py
@@ -0,0 +1,9 @@
+import sys
+import unittest
+
+class MockWorker(unittest.TestCase):
+    def test_worker_placeholder(self):
+        self.assertTrue(True)
+
+if __name__ == '__main__':
+    unittest.main()
